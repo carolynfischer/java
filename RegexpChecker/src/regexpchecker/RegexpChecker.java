23a12d4a5b79c54334ea31f67ff9cec648bab903
Carolyn Van Spyk <cvanspyk@gmail.com>
2016-11-13 21:41:22 -0800

    regular expression validity checker

---
+++ RegexpChecker/src/regexpchecker/RegexpChecker.java
@@ -0,0 +1,26 @@
+package regexpchecker;
+import java.util.Scanner;
+import java.util.regex.*;
+
+/**
+ *
+ * @author carry
+ * https://www.hackerrank.com/challenges/pattern-syntax-checker
+ */
+public class RegexpChecker {
+
+   public static void main(String[] args){
+      Scanner in = new Scanner(System.in);
+      int testCases = Integer.parseInt(in.nextLine());
+      while(testCases>0){
+         String pattern = in.nextLine();
+         try {
+            Pattern p = Pattern.compile(pattern);
+            System.out.println("Valid");
+         } catch (Exception e) {
+             System.out.println("Invalid");
+         }
+      }
+   }
+
+}
